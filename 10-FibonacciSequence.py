40fd6b232cd8abfd39e1bfa35ad395162dcdd85f
Khalil M. Najjar, PE <khalilnajjar@gmail.com>
2019-09-17 00:16:13 +0400

    Update 10-FibonacciSequence.py

--- 10-FibonacciSequence.py
+++ 10-FibonacciSequence.py
@@ -3,12 +3,15 @@
 # Link: https://docs.google.com/document/d/1TyqD2_oDtiQIh_Y55J5RfeA91JJECc97xYIKM112H9I/
 # Created by Khalil Najjar
 
-nth_digit = int(input("Please type the nth item in the Fibonacci sequence you would like to find: "))
+nth_digit = int(input("\n\nPlease type the nth item in the Fibonacci sequence you would like to find: "))
 
-digit = 0
 sequence = [0, 1]
+
+print(list(range(nth_digit)))
+
 for i in range(nth_digit):
-    sequence[i] += sequence[i-1]
+    sequence[i] = sequence[i-1] + sequence[i-2]
     sequence.append(sequence[i])
+
 print(sequence)
 print(sequence[nth_digit])
