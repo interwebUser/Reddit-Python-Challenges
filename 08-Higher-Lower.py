ad0a38ac7d6837be75cc284c970c2880a9b56a03
Khalil M. Najjar, PE <khalilnajjar@gmail.com>
2019-09-16 22:56:36 +0400

    Completed the 8th Challenge
    Created a game where the user guess a number selected by the computer between 1-100.

---
+++ 08-Higher-Lower.py
@@ -0,0 +1,33 @@
+import random
+
+print("\n\nWelcome to the Higher-Lower Guessing Game by Khalil Najjar!")
+print("The computer will select a random number between 1 and 100")
+print("You have to guess the number and the program will let you")
+print("know when you've selected the right number or if it's higher")
+print("than your guess. Let's get started!\n")
+
+def guessHigherLower():
+    numList = range(1,100)
+    number = random.choice(numList)
+    guessing = True
+    guess_count = 0
+    while guessing:
+        guess = int(input("Make a guess!\n"))
+        guess_count += 1
+        if guess == number:
+            print("You guessed the number " + str(number) + " correctly in "+ str(guess_count) + " tries!")
+            play_again = input("Would you like to play again? Y/N\n")
+            if play_again.upper() == "Y":
+                guessHigherLower()
+            else:
+                print("\nThanks for playing! Goodbye!\n")
+                exit()
+        else:
+            if guess > number:
+                print("Too high!\n")
+            if guess < number:
+                print("Too low!\n")
+
+
+
+guessHigherLower()
